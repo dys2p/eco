ac0b714c1106dbf191964a002006dacbb6d217e0
d2ro <78594032+d2ro@users.noreply.github.com>
2023-09-23 13:16:02 +0200

    refactor: use float64 for VAT calculations

--- countries/country.go
+++ countries/country.go
@@ -1,8 +1,6 @@
 // Package countries contains European countries and their VAT rates from an European Union point of view.
 package countries
 
-import "math"
-
 type Country struct {
 	ID       string // ISO 3166-1 code
 	Name     string // German
@@ -10,15 +8,15 @@ type Country struct {
 }
 
 // Gross returns the gross of the given net amount using the given VAT rate key. The boolean return value indicates if the rate has been found. If it is not found, the maximum rate is used.
-func (c Country) Gross(net int, rateKey string) (int, bool) {
+func (c Country) Gross(net float64, rateKey string) (float64, bool) {
 	rate, ok := c.VATRate(rateKey)
-	return int(math.Round(float64(net) * (1.0 + rate))), ok
+	return float64(net) * (1.0 + rate), ok
 }
 
 // Gross returns the net of the given gross amount using the given VAT rate key. The boolean return value indicates if the rate has been found. If it is not found, the maximum rate is used.
-func (c Country) Net(gross int, rateKey string) (int, bool) {
+func (c Country) Net(gross float64, rateKey string) (float64, bool) {
 	rate, ok := c.VATRate(rateKey)
-	return int(math.Round(float64(gross) / (1.0 + rate))), ok
+	return float64(gross) / (1.0 + rate), ok
 }
 
 // VATRate returns the VAT rate with the given key. The boolean return value indicates if the rate has been found. If it is not found, the maximum rate is used.
