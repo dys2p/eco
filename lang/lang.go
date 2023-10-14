01a959e5e0f034c96ff5e5c6616ea73eb6049119
d2ro <78594032+d2ro@users.noreply.github.com>
2023-10-14 14:47:33 +0200

    refactor: remove lang.Get

--- lang/lang.go
+++ lang/lang.go
@@ -2,26 +2,10 @@
 package lang
 
 import (
-	"net/http"
-	"strings"
-
-	"golang.org/x/exp/slices"
 	"golang.org/x/text/message"
 )
 
-var PathPrefixes = []string{"de", "en"}
-
 type Lang string
-
-// Get returns the first URL path item (if it is in PathPrefixes) or the HTTP "Accept-Language" header value.
-func Get(r *http.Request) Lang {
-	relpath := strings.TrimPrefix(r.URL.Path, "/")
-	pathprefix, _, _ := strings.Cut(relpath, "/")
-	if slices.Contains(PathPrefixes, pathprefix) {
-		return Lang(pathprefix)
-	}
-	return Lang(r.Header.Get("Accept-Language"))
-}
 
 // Tr translates the given input text.
 func (lang Lang) Tr(key message.Reference, a ...interface{}) string {
