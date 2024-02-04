a5eb3e74238d76f123301bc6cd808b0afef5d6e8
d2ro <78594032+d2ro@users.noreply.github.com>
2024-02-04 18:26:59 +0100

    feat: httputil.Origin

--- httputil/httputil.go
+++ httputil/httputil.go
@@ -3,9 +3,11 @@ package httputil
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -76,4 +78,15 @@ func ListenAndServe(addr string, handler http.Handler, stop chan os.Signal) func
 			log.Printf("error shutting down: %v", err)
 		}
 	}
+}
+
+// Origin returns the scheme plus host (including port, if available). The scheme is https, except when the host looks like localhost or a Tor hidden service.
+//
+// If you use nginx as a reverse proxy, make sure you have set "proxy_set_header Host $host;" besides proxy_pass in your configuration.
+func Origin(r *http.Request) string {
+	var proto = "https"
+	if strings.HasPrefix(r.Host, "127.0.") || strings.HasPrefix(r.Host, "[::1]") || strings.HasSuffix(r.Host, ".onion") || strings.Contains(r.Host, ".onion:") {
+		proto = "http"
+	}
+	return fmt.Sprintf("%s://%s", proto, r.Host)
 }
