82428696b9a4b4f7c27f111e98727788c0e25790
d2ro <78594032+d2ro@users.noreply.github.com>
2024-03-11 11:05:16 +0100

    fix: ssg example name

--- ssg/ssg_test.go
+++ ssg/ssg_test.go
@@ -8,7 +8,7 @@ import (
 
 //go:generate gotext-update-templates -srclang=en-US -lang=de-DE,en-US -out=catalog.go -d . -d ./example.com
 
-func ExampleStaticHTML() {
+func ExampleWebsite_StaticHTML() {
 	ssg.Must(ssg.MakeWebsite(os.DirFS("./example.com"), nil, "de", "en")).StaticHTML("/tmp/build/example.com")
 	ssg.ListenAndServe("/tmp/build/example.com")
 }
