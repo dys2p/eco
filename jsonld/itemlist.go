966dd30ca58061a227169f4e04df5f2ca9f89b85
d2ro <78594032+d2ro@users.noreply.github.com>
2024-06-16 01:19:56 +0200

    feat: package jsonld

---
+++ jsonld/itemlist.go
@@ -0,0 +1,44 @@
+package jsonld
+
+type ItemList struct {
+	Context  string            `json:"@context"`
+	Type     string            `json:"@type"`
+	Elements []ItemListElement `json:"itemListElement"`
+}
+
+type ItemListElement struct {
+	Type     string `json:"@type"`
+	Position int    `json:"position"` // starting with 1
+	Name     string `json:"name"`
+	Item     string `json:"item,omitempty"` // URL
+}
+
+type Breadcrumb interface {
+	Name() string
+	URLPath() string
+}
+
+// See https://developers.google.com/search/docs/appearance/structured-data/breadcrumb
+//
+// BreadcrumbList uses generics so we don't have to convert the slice from []SomeType to []Breadcrumb.
+func BreadcrumbList[T Breadcrumb](urlprefix string, breadcrumbs []T) ItemList {
+	var elements []ItemListElement
+	for i, breadcrumb := range breadcrumbs {
+		var url string
+		if path := breadcrumb.URLPath(); path != "" {
+			url = urlprefix + path
+		}
+
+		elements = append(elements, ItemListElement{
+			Type:     "ListItem",
+			Position: i + 1,
+			Name:     breadcrumb.Name(),
+			Item:     url,
+		})
+	}
+	return ItemList{
+		Context:  "https://schema.org",
+		Type:     "BreadcrumbList",
+		Elements: elements,
+	}
+}
---
+++ jsonld/itemlist_test.go
@@ -0,0 +1,32 @@
+package jsonld
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+type breadcrumb struct {
+	name    string
+	urlpath string
+}
+
+func (b breadcrumb) Name() string {
+	return b.name
+}
+
+func (b breadcrumb) URLPath() string {
+	return b.urlpath
+}
+
+func TestBreadcrumbList(t *testing.T) {
+	got, _ := json.Marshal(BreadcrumbList("https://example.com", []Breadcrumb{
+		breadcrumb{"Books", "/books"},
+		breadcrumb{"Science Fiction", "/books/sciencefiction"},
+		breadcrumb{"Award Winners", ""},
+	}))
+	want := `{"@context":"https://schema.org","@type":"BreadcrumbList","itemListElement":[{"@type":"ListItem","position":1,"name":"Books","item":"https://example.com/books"},{"@type":"ListItem","position":2,"name":"Science Fiction","item":"https://example.com/books/sciencefiction"},{"@type":"ListItem","position":3,"name":"Award Winners"}]}`
+
+	if string(got) != want {
+		t.Fatalf("got %s, want %s", got, want)
+	}
+}
