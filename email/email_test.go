e972bbed43baf93c8ad149eeb8e39a549f473afb
d2ro <78594032+d2ro@users.noreply.github.com>
2024-07-20 11:33:31 +0200

    test: email.TestMakeEmail

--- email/email_test.go
+++ email/email_test.go
@@ -1,6 +1,10 @@
 package email
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+	"time"
+)
 
 var emailers = []Emailer{
 	DummyMailer{},
@@ -33,5 +37,28 @@ func TestInvalidAddresses(t *testing.T) {
 				t.Fatalf("got %v, want %v", err, ErrInvalidAddress)
 			}
 		}
+	}
+}
+
+var messageID = regexp.MustCompile("[a-zA-Z0-9]{16}")
+
+func TestMakeEmail(t *testing.T) {
+	buf, _ := MakeEmail("alice@example.com", "bob@example.com", "Hello World", []byte("This is an example email."))
+	got := buf.String()
+	got = messageID.ReplaceAllString(got, "0123456789ABCDEF")
+
+	var want string
+	want += "MIME-Version: 1.0" + "\r\n"
+	want += "Content-Type: text/plain; charset=utf-8" + "\r\n"
+	want += "Date: " + time.Now().Format("02 Jan 2006 15:04:05 -0700") + "\r\n"
+	want += "Message-ID: <0123456789ABCDEF@example.com>" + "\r\n"
+	want += "From: alice@example.com" + "\r\n"
+	want += "Subject: Hello World" + "\r\n"
+	want += "To: bob@example.com" + "\r\n"
+	want += "\r\n"
+	want += "This is an example email."
+
+	if got != want {
+		t.Fatalf("got %s, want %s", got, want)
 	}
 }
