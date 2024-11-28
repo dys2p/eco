97e0de288b6955e360512bdaadddc8d8d380b816
d2ro <78594032+d2ro@users.noreply.github.com>
2024-11-28 21:14:24 +0100

    feat: cache health status
    The server has to be started now: go srv.Run()

--- payment/health/health.go
+++ payment/health/health.go
@@ -16,6 +16,7 @@ package health
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/dys2p/btcpay"
 	"github.com/dys2p/eco/payment/rates"
@@ -24,33 +25,34 @@ import (
 type Server struct {
 	BTCPay btcpay.Store
 	Rates  *rates.History
+	status []Item
 }
 
-func (srv Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	var response []Item
-
-	if srv.BTCPay != nil {
-		status, err := srv.BTCPay.GetServerStatus()
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+func (srv *Server) Run() {
+	for ; true; <-time.Tick(10 * time.Second) {
+		var status []Item
+		if srv.BTCPay != nil {
+			if serverStatus, err := srv.BTCPay.GetServerStatus(); err == nil {
+				for _, syncStatus := range serverStatus.SyncStatuses {
+					status = append(status, Item{
+						Name:   syncStatus.CryptoCode,
+						Synced: syncStatus.ChainHeight == syncStatus.SyncHeight,
+					})
+				}
+			}
 		}
-		for _, syncStatus := range status.SyncStatuses {
-			response = append(response, Item{
-				Name:   syncStatus.CryptoCode,
-				Synced: syncStatus.ChainHeight == syncStatus.SyncHeight,
+		if srv.Rates != nil {
+			status = append(status, Item{
+				Name:   "Foreign Cash",
+				Synced: srv.Rates.Synced(),
 			})
 		}
+		srv.status = status
 	}
+}
 
-	if srv.Rates != nil {
-		response = append(response, Item{
-			Name:   "Foreign Cash",
-			Synced: srv.Rates.Synced(),
-		})
-	}
-
-	responseData, _ := json.Marshal(response)
+func (srv *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	responseData, _ := json.Marshal(srv.status)
 	w.Header().Add("Content-Type", "application/json")
 	w.Write(responseData)
 }
