25e1d6f86d66f87cec206727fe3d9cb0ce9b2667
d2ro <78594032+d2ro@users.noreply.github.com>
2026-02-07 00:21:09 +0100

    style: go fmt

--- fmt_test.go
+++ fmt_test.go
@@ -3,14 +3,14 @@ package eco
 import "testing"
 
 func TestFmt(t *testing.T) {
-	tests := []struct{
-		got string
+	tests := []struct {
+		got  string
 		want string
 	}{
 		{FmtEuro(123), "1,23 €"},
 		{string(FmtEuroHTML(123)), "1,23&nbsp;€"},
-		{string(FmtEuroMinMaxHTML(123,123)), "1,23&nbsp;€"},
-		{string(FmtEuroMinMaxHTML(123,456)), "1,23&nbsp;–&nbsp;4,56&nbsp;€"},
+		{string(FmtEuroMinMaxHTML(123, 123)), "1,23&nbsp;€"},
+		{string(FmtEuroMinMaxHTML(123, 456)), "1,23&nbsp;–&nbsp;4,56&nbsp;€"},
 		{string(FmtEuroPlusMinusHTML(123)), "+1,23&nbsp;€"},
 		{string(FmtEuroPlusMinusHTML(-123)), "−1,23&nbsp;€"},
 		{string(FmtPercentHTML(0.12)), "12&nbsp;%"},
